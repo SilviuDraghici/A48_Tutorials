0546d20d60000d324c3a1b83127d7e8732fa489a
Silviu Draghici <silviu.dan.draghici@gmail.com>
2020-03-16 20:42:30 -0400

    added recursive solution to floodFill

--- 08/floodFill.c
+++ 08/floodFill.c
@@ -10,21 +10,22 @@ void printImgArray(char array[10][10])
  printf("------ Image Contents -------\n");
  for (int i=0; i<10; i++)
  {
+   printf("        |");
    for (int j=0; j<10; j++)
-     printf("%c, ",array[i][j]);
-   printf("\n");
+     printf("%c",array[i][j]);
+   printf("|\n");
  }
  printf("-----------------------------\n");
 }
 
-void floodFill(char image[10][10], int x, int y, char value, char bound)
+void floodFill(char image[10][10], int r, int c, char value, char bound)
 {
  // This function takes as input a 2D image array
  // which contains pixel values. 
  //
  // It floodFills (if you haven't tried floodfill,
  // open up a picture editor and use the paint tool!)
- // starting at location (x,y), filling the image
+ // starting at location (r,c), filling the image
  // with the specified 'value', up to (but not
  // including) the boundary of the shape in the
  // picture, whose colour is given by 'bound'
@@ -35,40 +36,72 @@ void floodFill(char image[10][10], int x, int y, char value, char bound)
 
 }
 
-void floodFill_R(char image[10][10], int x, int y, char value, char bound)
+void floodFill_R(char image[10][10], int r, int c, char value, char bound)
 {
  // This function takes as input a 2D image array
  // which contains pixel values. 
  //
  // It floodFills (if you haven't tried floodfill,
  // open up a picture editor and use the paint tool!)
- // starting at location (x,y), filling the image
+ // starting at location (r,c), filling the image
  // with the specified 'value', up to (but not
  // including) the boundary of the shape in the
  // picture, whose colour is given by 'bound'
  //
  // This function should do its work recursively!
+ 
+ 
+  if (r < 0 || r >= 10) return; //out of bounds
+  if (c < 0 || c >= 10) return; // out of bounds
+  if (image[r][c] == bound) return; // at chacter boundary
+  if (image[r][c] == value) return; // this stops it from infinitely going back
+                                    // and forth over the same area
+									// (stack overflow)
   
- // TO DO: Complete this function!
-
+  //printf("(%d, %d)\n", r, c); <- I used this to debug
+  image[r][c] = value;
+  
+  //These are the recursive calls, they 
+  floodFill_R(image, r - 1, c    , value, bound);
+  floodFill_R(image, r + 1, c    , value, bound);
+  floodFill_R(image, r    , c - 1, value, bound);
+  floodFill_R(image, r    , c + 1, value, bound);
+ 
 }
 
 int main()
 {
-  char polyImg[10][10]={{'_','_','_','_','_','_','_','_','_','_'},
-		               {'_','_','X','_','_','_','_','X','_','_'},
-		               {'_','X','_','X','_','_','X','_','X','_'},
-		               {'_','X','_','_','X','_','X','_','X','_'},
-		               {'_','_','X','_','X','X','_','_','X','_'},
-		               {'_','_','X','_','_','_','_','X','X','_'},
-		               {'_','X','_','_','_','X','_','X','_','_'},
-		               {'_','_','X','_','X','X','_','_','X','_'},
-		               {'_','_','X','X','_','_','X','X','X','_'},
-		               {'_','_','_','_','_','_','_','_','_','_'}};
-		       
+  char polyImg[10][10]={{' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+                        {' ',' ','X',' ',' ',' ',' ','X',' ',' '},
+                        {' ','X',' ','X',' ',' ','X',' ','X',' '},
+                        {' ','X',' ',' ','X',' ','X',' ','X',' '},
+                        {' ',' ','X',' ','X','X',' ',' ','X',' '},
+                        {' ',' ','X',' ',' ',' ',' ','X','X',' '},
+                        {' ','X',' ',' ',' ','X',' ','X',' ',' '},
+                        {' ',' ','X',' ','X','X',' ',' ','X',' '},
+                        {' ',' ',' ','X',' ',' ','X','X','X',' '},
+                        {' ',' ',' ',' ',' ',' ',' ',' ',' ',' '}};
+  printf("Original Image:\n");
+  printImgArray(polyImg);             
+  
+  floodFill_R(polyImg, 5, 4, 'a', 'X');
+  printf("\nFlood Fill starting at (5,4):\n");
   printImgArray(polyImg);
-  floodFill(polyImg,5,4,'a','X');
-  printImgArray(polyImg);
+  
+  char polyImg2[10][10]={{' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+                         {' ',' ','X',' ',' ',' ',' ','X',' ',' '},
+                         {' ','X',' ','X',' ',' ','X',' ','X',' '},
+                         {' ','X',' ',' ','X',' ','X',' ','X',' '},
+                         {' ',' ','X',' ','X','X',' ',' ','X',' '},
+                         {' ',' ','X',' ',' ',' ',' ','X','X',' '},
+                         {' ','X',' ',' ',' ','X',' ','X',' ',' '},
+                         {' ',' ','X',' ','X','X',' ',' ','X',' '},
+                         {' ',' ',' ','X',' ',' ','X','X','X',' '},
+                         {' ',' ',' ',' ',' ',' ',' ',' ',' ',' '}};
+  floodFill_R(polyImg2, 3, 0, 's', 'X');
+  printf("\nFlood Fill starting at (3,0):\n");
+  printImgArray(polyImg2);
+  
   
   return 0;
 }
